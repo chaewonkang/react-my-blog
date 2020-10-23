55ce24c571097e43df97db4124b4f8a7368a74eb
Chaewon Kang <ilpermet@gmail.com>
2020-10-23 19:42:17 +0900

    initial commit

---
+++ Untitled.sql
@@ -0,0 +1,5 @@
+create table test(
+	id INT(11) unsigned NOT NULL,
+	str VARCHAR(32) NOT NULL,
+    PRIMARY KEY (id)
+)
--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      host: "chaewon",
+      test: "",
+    };
+  }
+
+  componentDidMount() {
+    this._dbTest();
+  }
+
+  _dbTest = async () => {
+    const res = await axios.get("/api/test");
+    console.log(res.data);
+  };
+
+  /* axios로 /api/host에 get 요청 보내고, chaewon 으로 응답 결과 받기 */
+  /* 응답 받은 chaewon 값은 setState 함수를 통해 host state에 할당되고 화면 리렌더링 */
+  //   _getHost = async () => {
+  //     const res = await axios.get("/api/host");
+  //     this.setState({ host: res.data.host });
+  //   };
+
+  render() {
+    return (
+      <div className="App">
+        <h3>
+          {" "}
+          Welcome to <u> {this.state.host} </u> Blog!{" "}
+        </h3>
+      </div>
+    );
+  }
 }
 
 export default App;
